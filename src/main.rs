79e48e1e6736e0f6011e43f02adb95bcb3dd7c9d
Slimwolff <ptudoandtodos@gmail.com>
2024-11-28 17:21:45 -0300

    feat: add method get with client

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::Client;
+
+pub static URL: &str = "https://jsonplaceholder.typicode.com/posts/2";
+
+async fn get_response() {
+    let c: Client = Client::new();
+    let res = c.get(URL).send().await;
+    let body = match res {
+        Ok(r) => match r.text().await {
+            Ok(txt) => txt,
+            Err(e) => panic!("{:#?}", e),
+        },
+        Err(e) => panic!("{:#?}", e),
+    };
+    println!("Body: {:#?}", body);
+}
+
+#[tokio::main]
+async fn main() {
+    let _ = get_response().await;
 }
